d070057fc7d3a0c06c33642d368bfeebb69a5e5f
Seu Nome <seu.email@exemplo.com>
2025-03-27 09:08:43 -0300

    update

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import { Profile as ProfileComponent } from '../components/Profile';
+import { Layout } from '../components/Layout';
 
 export default function Profile() {
   return (
-    <div className="min-h-screen bg-gray-100 py-12">
-      <div className="container mx-auto px-4">
-        <ProfileComponent />
-      </div>
-    </div>
+    <Layout>
+      <ProfileComponent />
+    </Layout>
   );
 }
