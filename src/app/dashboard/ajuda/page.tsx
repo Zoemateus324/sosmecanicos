9c02cb9d664953be52639807bbb1b4922d31754c
Zoé Mateus <83597652+Zoemateus324@users.noreply.github.com>
2025-05-15 17:08:57 -0300

    Atualizar o page.tsx

--- src/app/dashboard/ajuda/page.tsx
+++ src/app/dashboard/ajuda/page.tsx
@@ -1,5 +1,12 @@
+'use client';
+
+import {Sidebar} forma '@/componentes/sidebar';
+
 export default function AjudaDashboard(){
     return(
+<div className="flex flex-col w-full" >
+        <Sidebar />
         <h1>Página de ajuda</h1>
+</div>
     )
 }
