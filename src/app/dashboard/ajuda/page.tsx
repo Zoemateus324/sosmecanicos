d01ef8897d09b1d333ffc0b3da293936c65a101b
Zoé Mateus <zmmateus2@gmail.com>
2025-05-30 13:31:44 -0300

    update

--- src/app/dashboard/ajuda/page.tsx
+++ src/app/dashboard/ajuda/page.tsx
@@ -4,15 +4,19 @@ import { Sidebar } from '@/components/sidebar/Sidebar';
 
 
 
+import React, { useState } from 'react';
+
 export default function AjudaDashboard() {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+  const toggleSidebar = () => setIsSidebarOpen((prev) => !prev);
+
   return (
     <div className="flex flex-col">
-      <Sidebar />
+       <Sidebar isOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />
       <main className='flex-1  container mx-auto p-4 flex justify-center items-start w-full'>
         <h2>Sua página de suporte e ajuda!</h2>
       </main>
     </div>
-
-
   );
 }
